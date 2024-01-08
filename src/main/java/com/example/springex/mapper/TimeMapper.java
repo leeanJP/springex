bc0b7f7c2ac46c819846f761fa14350016edcdf0
leean <zzzttt9@gmail.com>
2024-01-08 11:50:13 +0900

    Initial commit

---
+++ src/main/java/com/example/springex/mapper/TimeMapper.java
@@ -0,0 +1,10 @@
+package com.example.springex.mapper;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+
+@Mapper
+public interface TimeMapper {
+    @Select("select now()")
+    String getTime();
+}
